36b2ea845c93d8895bda21f33f10fc5dd3e733ea
[Raj Sah]} <[rajsah@gmail.com]}>
2016-06-10 09:46:23 +0530

    added config file and gulpfile
    compass is not added yet

---
+++ config.rb
@@ -0,0 +1,13 @@
+require "susy"
+
+project_type = :stand_alone
+http:_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compressed
+relative_assets = true
